4cf9aa0e6ba0531cde996e82c4008c4241aa30ea
hbleie <92336221+hbleie@users.noreply.github.com>
2022-03-26 14:14:19 +0100

    Finnsøndag lagt til

--- example.py
+++ example.py
@@ -16,12 +16,6 @@ tekst3 = fil.readline()
 dato = dato.split(" ")
 dato[2] = dato[2].replace("\n", "")
 
-s.title("Vaskeliste")
-
-s.header(tekst1)
-s.header(tekst2)
-s.header(tekst3)
-
 def sistoppdatering(idag, siste):
     today_date = date(int(idag[0]), int(idag[1]), int(idag[2]))
     siste_date = date(int(siste[0]), int(siste[1]), int(siste[2]))
@@ -46,7 +40,6 @@ def finnSøndag():
     else:
         return date.today()
 
-
 if sistoppdatering(today, dato) > 7:
     navn = ['Gard', 'Vemund', 'Håkon']
     rom = ['Kjøkken, gang', 'Bad', 'Stue']
@@ -59,13 +52,12 @@ if sistoppdatering(today, dato) > 7:
     vask = list(zip(navn, rom, søppel))
 
     fil = open("vaskeliste.txt", "w")
-
+    
     søndag = str(finnSøndag()).split("-")
     tekst1 = vask[0][0] + ': ' + vask[0][1] + ' og ' + vask[0][2]
     tekst2 = vask[1][0] + ': ' + vask[1][1] + ' og ' + vask[1][2]
     tekst3 = vask[2][0] + ': ' + vask[2][1] + ' og ' + vask[2][2]
 
     fil.write(søndag[0] + ' '  + søndag[1] + ' ' + søndag[2] + "\n" + tekst1 + "\n" + tekst2 + "\n" + tekst3)
-    
-    
-
+        
+fil.close()
