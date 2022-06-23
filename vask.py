0e33849453486798fde2ee7c8f5cd1cb3570890d
hbleie <92336221+hbleie@users.noreply.github.com>
2022-06-23 21:38:21 +0200

    Update vask.py

--- vask.py
+++ vask.py
@@ -68,7 +68,7 @@ def finnSøndag():
 
 
 if sistoppdatering(today, dato) > 7:
-    navn = ['Ola', 'Vemund', 'Håkon']
+    navn = ['Nymann', 'Vemund', 'Håkon']
     rom = ['Kjøkken, gang', 'Bad', 'Stue']
     søppel = ['Glass', 'Papp', 'Pant']
 
